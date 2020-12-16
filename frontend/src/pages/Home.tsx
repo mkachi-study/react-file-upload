5aa73005b39358860fc02cdbb40c13d67b31541b
MKachi <mkachi@naver.com>
2020-12-17 01:00:01 +0900

    feat: 파일 업로드 page 추가

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
-  return <p>{'Home'}</p>
+  const [content, setContent] = useState(null) // 업로드 전용
+  const [image, setImage] = useState('') // base64
+  const [imageUri, setImageUri] = useState('') // server response
+
+  return <div>
+    <form onSubmit={async (event) => {
+      try {
+        event.preventDefault()
+        const formData = new FormData()
+        formData.append('img', content)
+
+        const response = await fetch('http://localhost:9003/upload', {
+          method: 'POST',
+          body: formData
+        })
+        const json = await response.json()
+        if (json.success) {
+          setImageUri(json.data)
+        } else {
+          console.error(json.data)
+        }
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }}>
+      <input type={'file'} accept={'image/*'} onChange={(event) => {
+        let reader = new FileReader()
+        reader.onloadend = () => {
+          const base64 = reader.result.toString()
+          if (base64) {
+            setImage(base64)
+          }
+        }
+
+        if (event.target.files[0]) {
+          setContent(event.target.files[0])
+          reader.readAsDataURL(event.target.files[0])
+        }
+      }} />
+      <br/>
+      <input type={'submit'}/>
+    </form>
+    <p>
+      <img src={image} />
+    </p>
+    <p>
+      <img src={imageUri} />
+    </p>
+    <p>{image}</p>
+  </div>
 }
 
 export default Home
